92226a167f3d6d4cab083afa9abe803efea9a5da
Kriss071 <chrisquinga@gmail.com>
2024-05-30 13:56:59 -0400

    URLs corregido

--- products/urls.py
+++ products/urls.py
@@ -5,6 +5,8 @@ from . import views
 router = routers.DefaultRouter()
 router.register('api/products', views.ProductViewSet, 'products')
 router.register('api/categories', views.CategoryViewSet, 'categories')
+router.register('api/pedidos', views.PedidoViewSet, 'pedido')
+router.register('api/detalle', views.DetallePedidoViewSet, 'detalle')
 
 urlpatterns = router.urls + [
     path('products/', views.product_list, name='product_list'),
